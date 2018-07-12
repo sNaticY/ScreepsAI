11ecf5683f636cd967b4914842b1d95c2c6b6a43
sNatic <snatic@outlook.com>
2018-07-13 01:40:09 +0200

    Add basically creep library

---
+++ src/screeps/CreepLibaray.ts
@@ -0,0 +1,47 @@
+import { Dictionary } from "lodash";
+import { CreepPrototype, RoleTypeString, TaskTypeString } from "./TypeDefs";
+
+export class CreepLibaray {
+    public static Prototypes: Dictionary<CreepPrototype[]> = {
+        [TaskTypeString.Harvest]: [
+            { // Level1
+                taskType: TaskTypeString.Harvest,
+                econemyLevel: 1, workPoint: 4, level: 1,
+                body: [
+                    WORK, WORK, CARRY, MOVE
+                ],
+                role: RoleTypeString.Harvester
+            }
+        ],
+        [TaskTypeString.Carry]: [
+            { // Level1
+                taskType: TaskTypeString.Carry,
+                econemyLevel: 1, workPoint: 100, level: 1,
+                body: [
+                    CARRY, CARRY, MOVE, MOVE
+                ],
+                role: RoleTypeString.Harvester
+            }
+        ],
+        [TaskTypeString.Build]: [
+            { // Level1
+                taskType: TaskTypeString.Build,
+                econemyLevel: 1, workPoint: 1, level: 1,
+                body: [
+                    WORK, CARRY, MOVE, MOVE
+                ],
+                role: RoleTypeString.Harvester
+            }
+        ],
+        [TaskTypeString.Upgrade]: [
+            { // Level1
+                taskType: TaskTypeString.Upgrade,
+                econemyLevel: 1, workPoint: 1, level: 1,
+                body: [
+                    WORK, CARRY, MOVE, MOVE
+                ],
+                role: RoleTypeString.Harvester
+            }
+        ]
+    };
+}
